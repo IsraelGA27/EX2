3e0ab72bd2797c9971b4351a9198e69726784ce1
Israel García Agüero <ig239503@gmail.com>
2022-06-09 13:44:29 -0500

    Iniciando con label de moneda y combobox

--- Form1.cs
+++ Form1.cs
@@ -26,6 +26,32 @@ namespace EX2
             InicializarComponentes();
         }
 
+        private void InicializarComponentes()
+        {
+            // tamaño para la vnetana inicial
+            this.Size = new Size(300, 350);
+
+            // label moneda
+            lblFigura = new Label();
+            lblFigura.Text = "Moneda";
+            lblFigura.AutoSize = true;
+            lblFigura.Location = new Point(10, 10);
+
+            // combobox Moneda(s)
+            cmbFiguras = new ComboBox();
+            cmbFiguras.Items.Add("Selecciona Moneda");
+            cmbFiguras.Items.Add("MXN (Peso Mexicano)");
+            cmbFiguras.Items.Add("USD (Dolar Estadounidense)");
+            cmbFiguras.Items.Add("CAD (Dolar Canadiense)");
+            cmbFiguras.Items.Add("EUR (Euro)");
+            cmbFiguras.Items.Add("JPY (Yen Japones)");
+
+            cmbFiguras.SelectedIndex = 0;
+            cmbFiguras.Location = new Point(10, 40);
+            cmbFiguras.SelectedValueChanged += new EventHandler(cmb_ValueChanged);
+
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
